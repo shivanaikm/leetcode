4ac6f7d6acce3aa512d265b738824135fee2661f
shivanaikm <shivanaikm18@gmail.com>
2025-09-14 22:51:55 +0530

    0048-rotate-image.java

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -17,3 +17,28 @@ class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    public void rotate(int[][] matrix) {
+       for(int i=0;i<matrix.length;i++){
+        for(int j=i+1;j<matrix[i].length;j++){
+            int temp=matrix[i][j];
+            matrix[i][j]=matrix[j][i];
+            matrix[j][i]=temp;
+        }
+       }
+        for(int i=0;i<matrix.length;i++){
+            int left=0,right=matrix[0].length-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+       } 
+       System.out.print(Arrays.toString(matrix));
+    }
+}
+*/
